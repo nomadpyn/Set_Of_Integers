424cbfbbc38f1705562d3a2543ccdf0d4b90abac
Alexey <105908089+nomadpyn@users.noreply.github.com>
2022-07-01 00:44:28 +0700

    Commit 5
    Make default constructor
    Make destructor

--- set_of_integers/SoI.h
+++ set_of_integers/SoI.h
@@ -6,16 +6,20 @@ private:
 	unsigned int* arr;
 public:
 	SoI(const int szP) :size{ szP } {
-		arr = new unsigned int[size] {};
-		cout << "You create array with " << size << " elements.\n";
+		this->arr = new unsigned int[this->size] {};
+		cout << "You create array with " << this->size << " elements.\n";
 		cout << "To make Set of Integers you need fill this array\n";
 	}
 	SoI (const unsigned int *arrP, const int szP) : size{ szP } {
-		arr = new unsigned int[size] {};
-		for (int i = 0; i < size; i++)
-			arr[i] = arrP[i];
-		cout << "You create array with " << size << " elements.\n";
+		this->arr = new unsigned int[this->size] {};
+		for (int i = 0; i < this->size; i++)
+			this->arr[i] = arrP[i];
+		cout << "You create array with " << this->size << " elements.\n";
 		cout << "To make Set of Integers we need check this array for coincidence.\n";
+	}
+	SoI():SoI(3){}
+	~SoI() {
+		delete[] this->arr;
 	}
 };
 
