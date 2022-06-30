dc8c050afcfa3f1d9769cacd9be8929b8b946204
Alexey <105908089+nomadpyn@users.noreply.github.com>
2022-07-01 00:41:13 +0700

    Commit 4
    Make constructor with param

--- set_of_integers/SoI.h
+++ set_of_integers/SoI.h
@@ -2,7 +2,20 @@
 class SoI
 {
 private:
-	unsigned int size;
+	int size;
 	unsigned int* arr;
+public:
+	SoI(const int szP) :size{ szP } {
+		arr = new unsigned int[size] {};
+		cout << "You create array with " << size << " elements.\n";
+		cout << "To make Set of Integers you need fill this array\n";
+	}
+	SoI (const unsigned int *arrP, const int szP) : size{ szP } {
+		arr = new unsigned int[size] {};
+		for (int i = 0; i < size; i++)
+			arr[i] = arrP[i];
+		cout << "You create array with " << size << " elements.\n";
+		cout << "To make Set of Integers we need check this array for coincidence.\n";
+	}
 };
 
